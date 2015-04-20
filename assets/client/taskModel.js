0252f5e04109b26d511c1be24ca4a9c79239c678
Josh Haber <haber.joshua@gmail.com>
2015-04-20 13:54:20 -0400

    bug(task.stop): fixed bug which updates stop times for all tasks when new task starts
    - tasks that were previously stopped would have their time extended to the start of the next task
    - added condition to only update stop time if no stop time existed previously
    
    #832800

--- assets/client/taskModel.js
+++ assets/client/taskModel.js
@@ -36,9 +36,14 @@ TaskModel.prototype.addTask = function (task) {
 
 TaskModel.prototype.stop = function (taskToStop) {
 	this.tasks = this.tasks.map(function (task) {
-		return task !== taskToStop ?
-			task :
-			Utils.extend({}, task, {stopTime: Moment().format() });
+		if(task === taskToStop)
+		{	
+			if (!task.stopTime){
+				document.getElementById(task.id + "-stop-time-edit").value = Moment().format('HH:mm:ss DD/MM/YY');
+				return Utils.extend({}, task, {stopTime: Moment().format() });
+			}
+		}
+		return task;
 	});
 
 	this.inform();
