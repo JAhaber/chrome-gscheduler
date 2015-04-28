c6023b62229e6b199e5a05e8a2b10a8b7bd8c532
Josh Haber <haber.joshua@gmail.com>
2015-04-28 14:45:19 -0400

    feat(sequencing): Added sequencing functionality and options
    - Can now choose sequenced or actual on options page
    - Updated API to save to genome based on option selected
    
    #832800

--- app/options.js
+++ app/options.js
@@ -1,14 +1,10 @@
-var isSelected = false;
-var ctrlalt = false;
 
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var color = $('#color').value;
-  alert(color);
-  var likesColor = document.getElementById('like').checked;
+  var type = document.querySelector('input[name="type"]:checked').value;
+
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    saveType: type
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -24,94 +20,20 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    saveType: 'Sequenced'
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    var radioType = document.getElementsByName('type');
+    for (i = 0; i < radioType.length; i++) {
+        if ( radioType[i].value === items.saveType ) {
+          radioType[i].checked = true;
+          break;
+        }
+    }
+    
   });
 }
-
-function callFocus(a){
-  a.select();
-  ctrlalt=false;
-  isSelected = true;
-}
-
-var hotkey = document.getElementById('hotkey');
-hotkey.addEventListener("keydown", function(event){
-  event.preventDefault();
-  if (isSelected)
-  {
-    isSelected = false;
-    hotkey.value = "";
-  }
-  var key = "";
-  switch(event.keyCode){
-    case 17: 
-      key = "Ctrl";
-      break;
-    case 18: 
-      key = "Alt";
-      break;
-    case 16: 
-      key = "Shift";
-      break;
-    case 32: 
-      key = "Space";
-      break;
-    case 33: 
-      key = "PageUp";
-      break;
-    case 34: 
-      key = "PageDown";
-      break;
-    case 35: 
-      key = "End";
-      break;
-    case 36: 
-      key = "Home";
-      break;
-    case 37: 
-      key = "Left";
-      break;
-    case 38: 
-      key = "Up";
-      break;
-    case 39: 
-      key = "Down";
-      break;
-    case 40: 
-      key = "Right";
-      break;
-    case 45: 
-      key = "Insert";
-      break;
-    case 46: 
-      key = "Delete";
-      break;
-    case 188: 
-      key = "Comma";
-      break;
-    case 190: 
-      key = "Period";
-      break;
-    default:
-      key = String.fromCharCode(event.keyCode);
-  }
-  
-  if ((key === "Alt" || key === "Ctrl") && ctrlalt === true)
-    key = "";
-  else
-    ctrlalt = true;
-
-  if (!(hotkey.value === ""))
-    hotkey.value = hotkey.value + "+";
-  hotkey.value = hotkey.value + event.keyCode;
-});
 
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
-
