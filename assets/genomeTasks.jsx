7150aac2c5964fa17e72a3244ea55b9dfa8d1eff
Josh Haber <haber.joshua@gmail.com>
2015-05-28 16:13:18 -0400

    feat(options): added option to show/hide the GScheduler button in Genome
    #832800

--- assets/genomeTasks.jsx
+++ assets/genomeTasks.jsx
@@ -6,44 +6,49 @@ var imgURL = chrome.extension.getURL("images/icon-19.png");
 var ticketID;
 var title;
 var projectID;
+var showButton = true;
 var style = "left:" + ($(window).width()/2 - 75) + "px;";
 
 function addGButton(){
-	ticketID = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
-	GenomeAPI.getProjectInfo(ticketID).then(function(ticketData){
+	if (showButton === true){
+
+		ticketID = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
 		
-	        if (!(ticketData.Entries[0].TicketStatusName === "closed"))
-	        { 
-	        	GButton = "<div id='gButton' style='"+ style + "'>" +
-	        	"<img src='" + imgURL + "'>"+
-	        	" Track in GScheduler</div>";
+		GenomeAPI.getProjectInfo(ticketID).then(function(ticketData){
+			
+		        if (!(ticketData.Entries[0].TicketStatusName === "closed"))
+		        { 
+		        	GButton = "<div id='gButton' style='"+ style + "'>" +
+		        	"<img src='" + imgURL + "'>"+
+		        	" Track in GScheduler</div>";
 
-	        	$("body").append(GButton);
-	        	title = ticketData.Entries[0].Title;
-	        	projectID = ticketData.Entries[0].ProjectID;
-	        }
-	        else
-	        	console.log(ticketData.Entries[0].TicketStatusName);
-	      }).fail(function(error){
-	        console.log(error);
-	      });
+		        	$("body").append(GButton);
+		        	title = ticketData.Entries[0].Title;
+		        	projectID = ticketData.Entries[0].ProjectID;
+		        }
+		        else
+		        	console.log(ticketData.Entries[0].TicketStatusName);
+		      }).fail(function(error){
+		        console.log(error);
+		      });
 
 
-	$("body").on("click", "#gButton", function(){
-		
-		chrome.runtime.sendMessage({
-			newTask: {
-				ticketID: ticketID,
-				title: title,
-				projectID: projectID,
-				categoryID: null
-			}
-		}, function(response) {});
-	});
+		$("body").on("click", "#gButton", function(){
+			
+			chrome.runtime.sendMessage({
+				newTask: {
+					ticketID: ticketID,
+					title: title,
+					projectID: projectID,
+					categoryID: null
+				}
+			}, function(response) {});
+		});
 
-	$( window ).resize(function(){
-		$("#gButton").css("left", ($(window).width()/2 - 75) + "px");
-	});
+		$( window ).resize(function(){
+			$("#gButton").css("left", ($(window).width()/2 - 75) + "px");
+		});
+	}
 }
 
 $( window ).on("hashchange", function() {
@@ -51,7 +56,20 @@ $( window ).on("hashchange", function() {
 	addGButton();
 });
 
-addGButton();
+chrome.storage.sync.get({
+    genomeTask: true
+  }, function(items) {
+    showButton = items.genomeTask;
+    addGButton();
+  });
+
+chrome.storage.onChanged.addListener(function(changes, namespace){
+  chrome.storage.sync.get({
+     genomeTask: true
+  }, function(items) {
+    showButton = items.genomeTask;
+  });
+});
 
 
 
